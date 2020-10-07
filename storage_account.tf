faffc8d6852029ab558ca2f8920c68651ed36859
Ryo Nakamine <rnakamine8080@gmail.com>
2020-10-07 21:47:06 +0900

    storage account change tier

--- storage_account.tf
+++ storage_account.tf
@@ -3,5 +3,5 @@ resource "azurerm_storage_account" "sample" {
   resource_group_name      = azurerm_resource_group.sample.name
   location                 = azurerm_resource_group.sample.location
   account_tier             = "standard"
-  account_replication_type = "ZRS"
+  account_replication_type = "LRS"
 }
